20ff42c11a406adddba09ce0e0cf335b512c9f8c
Cole Pilegard <c.pilegard@gmail.com>
2013-11-16 13:55:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodForest::Application.config.secret_key_base = 'd1a8cfd44260a24583e7cbda046a84f1231fd8f3aa5a4b50e39d1083851673307d763cee3040148a2182c845611936a5b1e833815dfc8106259cabedc1842db2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodForest::Application.config.session_store :cookie_store, key: '_food-forest_session'
