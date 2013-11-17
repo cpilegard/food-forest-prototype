ea4208144ae68b5fdbdc338d9f40fb8611b48b67
Cole Pilegard <c.pilegard@gmail.com>
2013-11-17 12:15:15 -0800

    clicking on task will add my face to the card and remove the unfulfilled status

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,4 @@
+$('.unfulfilled').on('click', function(e) {
+  $(this).removeClass('unfulfilled');
+  $(this).append("<div class='avatar'><img class='image' src='http://www.gravatar.com/avatar/b64488fa3db5b2d472e1bc7e23325a30?s=40'></div>");
+});
