601e8c2f102ff70b07e5e231f6201de8f169dfea
Nicholas Wargnier <nwargnier@gmail.com>
2013-11-16 16:50:53 -0800

    delete bad table

--- db/migrate/20131116235843_create_user_resources.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserResources < ActiveRecord::Migration
-  def change
-    create_table :user_resources do |t|
-      t.references :user
-      t.references :resource
-    end
-  end
-end
